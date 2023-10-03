fcc629f7d1de94c1b87eaba0ced524f01615633c
Abhishek123540 <abhidas977@gmail.com>
2023-10-03 09:16:33 +0530

    master

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:riverrr_poddd/screens/categories.dart';
+import 'package:riverrr_poddd/screens/meals.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -8,8 +10,43 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  void _selectPage(int i) {
+    setState(() {
+      _selectedPageIndex = i;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget activePage = const CategoriesScreen();
+    var activePageTitle = 'Categories';
+    if (_selectedPageIndex == 1) {
+      activePage = const MealsScreen(
+        title: 'Favourites',
+        meals: [],
+      );
+      activePageTitle = 'Your Favourites';
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
   }
 }
